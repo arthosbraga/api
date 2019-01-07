25a1e194c6b03929f3716ad3a43342f4f5524108
ARTHOS PEREIRA BRAGA <arthoscs@gmail.com>
2019-01-07 12:14:48 -0200

    Proto

---
+++ apps/config.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+# Python
+from os import getenv
+
+
+class Config:
+    SECRET_KEY = getenv('SECRET_KEY') or 'uma string randômica e gigante'
+    APP_PORT = int(getenv('APP_PORT'))
+    DEBUG = eval(getenv('DEBUG').title())
+
+
+class DevelopmentConfig(Config):
+    FLASK_ENV = 'development'
+    DEBUG = True
+
+class TestingConfig(Config):
+    FLASK_ENV = 'testing'
+    TESTING = True
+
+config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'default': DevelopmentConfig
+}
